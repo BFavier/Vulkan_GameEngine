7de0c95e26225170e824d744b06abdfab4ef245e
BenoitFavier <bade@webmails.top>
2021-10-30 15:06:36 +0200

    removed .o and .d from project

--- obj/GameEngine/user_interface/Timer.d
+++
@@ -1,3 +0,0 @@
-obj/GameEngine/user_interface/Timer.o: \
- src/GameEngine/user_interface/Timer.cpp \
- include/GameEngine/user_interface/Timer.hpp
--- obj/GameEngine/utilities/Functions.d
+++
@@ -1,4 +0,0 @@
-obj/GameEngine/utilities/Functions.o: \
- src/GameEngine/utilities/Functions.cpp \
- include/GameEngine/utilities/Functions.hpp \
- include/GameEngine/utilities/Macro.hpp
