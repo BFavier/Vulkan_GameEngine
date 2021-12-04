4d4fe013c702deb755202ca4c91e72ccb15f5eca
BenoitFavier <bade@webmails.top>
2021-12-04 18:27:32 +0100

    added Shader class

--- include/GameEngine/graphics/Shader.hpp
+++ include/GameEngine/graphics/Shader.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "GPU.hpp"
 #include <GameEngine/utilities/Macro.hpp>
 #include <vector>
 #include <string>
@@ -10,8 +11,16 @@ namespace GameEngine
     {
     public:
         Shader() = delete;
+        Shader(const Shader& other);
+        Shader(const GPU& gpu, const std::string& file_path);
+        Shader(const GPU& gpu, const std::vector<unsigned char>& code);
         ~Shader();
+    public:
+        const GPU& gpu;
+        VkShaderModule _vk_shader;
+    public:
+        static std::vector<unsigned char> load_binary(const std::string& file_path);
     protected:
-        std::vector<unsigned char> _load_binary(const std::string& file_path) const;
+        void _set_vk_shader(const std::vector<unsigned char>& code);
     };
 }
--- src/GameEngine/graphics/Shader.cpp
+++ src/GameEngine/graphics/Shader.cpp
@@ -1,11 +1,44 @@
 #include <GameEngine/graphics/Shader.hpp>
 using namespace GameEngine;
 
-std::vector<unsigned char> Shader::_load_binary(const std::string& file_path) const
+Shader::Shader(const Shader& other) : gpu(other.gpu)
+{
+    _vk_shader = other._vk_shader;
+}
+
+Shader::Shader(const GPU& _gpu, const std::string& file_path) : gpu(_gpu)
+{
+    _set_vk_shader(load_binary(file_path));
+}
+
+Shader::Shader(const GPU& _gpu, const std::vector<unsigned char>& code) : gpu(_gpu)
+{
+    _set_vk_shader(code);
+}
+
+std::vector<unsigned char> Shader::load_binary(const std::string& file_path)
 {
     std::ifstream file(file_path, std::ios::ate | std::ios::binary);
     if (!file.is_open())
     {
         THROW_ERROR("failed to open file")
+    }
+    size_t fileSize = static_cast<size_t>(file.tellg());
+    std::vector<unsigned char> buffer(fileSize);
+    file.seekg(0);
+    file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
+    file.close();
+    return buffer;
+}
+
+void Shader::_set_vk_shader(const std::vector<unsigned char>& code)
+{
+    VkShaderModuleCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    createInfo.codeSize = code.size();
+    createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+    if (vkCreateShaderModule(gpu._logical_device, &createInfo, nullptr, &_vk_shader) != VK_SUCCESS)
+    {
+        THROW_ERROR("failed to create shader module")
     }
 }
